1e73f68e31534747272404caa68594b7c87dbda4
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-16 14:58:36 -0400

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testes do componente \'NotFound\'', () => {
+  it('verifica se ao renderizar o \'NotFound\' o titulo esta na tela', () => {
+    renderWithRouter(<NotFound />);
+
+    const messageNotFound = screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2 });
+    expect(messageNotFound).toBeInTheDocument();
+  });
+
+  it('verifica se ao renderizar o \'NotFound\' a imagem esta na tela', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
