5bda121ab19577aca521252a1e8c5f30df5a3068
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-16 14:53:20 -0400

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testes do componente \'FavoritePokemons\'', () => {
+  it('verifica se ao renderizar o \'About\' o titulo esta na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const favoriteTitle = screen.getByRole('heading', {
+      name: 'Favorite pokémons', level: 2 });
+    expect(favoriteTitle).toBeInTheDocument();
+  });
+
+  it('verifica se ao renderizar o \'FavoritePokemons\' aparece mssg: esta vazio', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
