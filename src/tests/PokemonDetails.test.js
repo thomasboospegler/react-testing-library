4c6e1713f4ff825c90ce16d57fd2b8dc1869831e
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-16 16:27:29 -0400

    Requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import PokemonDetails from '../pages/PokemonDetails';
+import pokemons from '../data';
+
+describe('Testes do componente \'PokemonDetails\'', () => {
+  it('verifica se ao renderizar o \'PokemonDetails\' pokemonInfo aparece na tela', () => {
+    renderWithRouter(<PokemonDetails
+      match={ { params: { id: 25 } } }
+      isPokemonFavoriteById
+      pokemons={ pokemons }
+    />);
+
+    const pokemonName = screen.getByRole('heading', {
+      name: 'Pikachu Details', level: 2 });
+    const pokemonSummary = screen.getByRole('heading', {
+      name: 'Summary', level: 2 });
+    const summaryText = screen.getByText(/Pokémon roasts hard berries with electricity/i);
+    const pokemonLocations = screen.getByRole('heading', {
+      name: 'Game Locations of Pikachu', level: 2 });
+    const pokemonImgs = screen.getAllByAltText('Pikachu location');
+
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonSummary).toBeInTheDocument();
+    expect(summaryText).toBeInTheDocument();
+    expect(pokemonLocations).toBeInTheDocument();
+    expect(pokemonImgs[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(pokemonImgs[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
+
+  it('verifica se ao renderizar o \'PokemonDetails\' o checkbox aparece na tela', () => {
+    renderWithRouter(<PokemonDetails
+      match={ { params: { id: 25 } } }
+      isPokemonFavoriteById
+      pokemons={ pokemons }
+    />);
+
+    const checkboxFav = screen.getByLabelText('Pokémon favoritado?');
+    expect(checkboxFav).toBeInTheDocument();
+  });
+});
