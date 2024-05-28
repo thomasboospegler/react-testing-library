46f302bb21663e610f9f6f0be43dfcd9cfc303ab
Thomas Boos Pegler <tboospegler@gmail.com>
2024-05-28 15:55:33 -0400

    .

---
+++ src/pages/Location.js
@@ -0,0 +1,10 @@
+// https://pokeapi.co/api/v2/location
+import React, { Component } from 'react';
+
+export default class Location extends Component {
+  render() {
+    return (
+      <div>Location</div>
+    );
+  }
+}
